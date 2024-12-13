3c383a5568291e164f2fe82f14fadf53bfb704ac
Priyesh Mishra <mpriyesh235@gmail.com>
2024-12-13 23:49:06 +0530

    improved logic of ascOrDescCheck.c code

--- Without Pointers/AscOrDescCheck.c
+++ Without Pointers/AscOrDescCheck.c
@@ -1,57 +1,40 @@
 #include <stdio.h>
+#include <stdbool.h>
 int main()
 {
     int size;
     printf("Enter the size of array: ");
-    scanf("%d", &size);
+    if(scanf("%d", &size) !=1)
+    {
+        printf("Enter valid size of array! Terminating process");
+        return 1;
+    }
+        
     int arr[size];
+    bool isAsc = 0, isDesc = 0;
+
     for (int i = 0; i < size; i++)
     {
         printf("%dth element is: ", i + 1);
-        scanf("%d", &arr[i]);
+        if(scanf("%d", &arr[i]) != 1)
+        {
+            printf("Input only interger value");
+            return 2;
+        }
     }
     for (int i = 0; i < size - 1; i++)
     {
-        if (arr[i] < arr[i + 1])
-        {
-            if (arr[i - 1] > arr[i])
-            {
-                printf("No Order");
-                return 0;
-            }
-            if (i == size - 2)
-            {
-                printf("ascending ");
-            }
-        }
-        else
-        {
-            if (arr[i] > arr[i + 1])
-            {
-                if (i == 0)
-                {
-                    if (arr[0] < arr[1])
-                    {
-                        printf("No Order");
-                        return 0;
-                    }
-                }
-                if (i > 0)
-                {
-                    if (arr[i - 1] < arr[i])
-                    {
-                        printf("No Order");
-                        return 0;
-                    }
-                }
-
-                if (i == size - 2)
-                {
-                    printf("descending ");
-                }
-            }
-        }
+        if (arr[i] < arr[i + 1]) isAsc = 1;
+        else isDesc = 1; 
     }
+
+    if (isAsc && isDesc)
+    {
+        printf("No Order");
+        return 3;
+    }
+    if (isAsc) printf("Ascending order");
+    else if (isDesc) printf("Descending order");
 
     return 0;
 }
--- Without Pointers/sortingNumsPosOrNeg.c
+++ Without Pointers/sortingNumsPosOrNeg.c
@@ -1,26 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-    int n, posCount=0,negCount=0,zeroCount=0, num[n], positive[n], negative[n],zeros[n];
+    int n, posCount = 0, negCount = 0, zeroCount = 0;
     printf("How many numbers you want to compare?");
-    scanf("%d",&n);
+    scanf("%d", &n);
+    int num[n], positive[n], negative[n], zeros[n];
 
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        printf("Enter the %dth number: ",i+1);
-        scanf("%d",&num[i]);
+        printf("Enter the %dth number: ", i + 1);
+        scanf("%d", &num[i]);
 
-        if(num[i]>0) positive[posCount++]=num[i];
-        else if(num[i]<0) negative[negCount++]=num[i];
-        else zeros[zeroCount++]=num[i];
+        if (num[i] > 0)
+            positive[posCount++] = num[i];
+        else if (num[i] < 0)
+            negative[negCount++] = num[i];
+        else
+            zeros[zeroCount++] = num[i];
     }
 
-    printf("\nTotal postive numbers=%d\n",posCount);
-    for(int i=0;i<posCount;i++) printf(" %d,",positive[i]);
+    printf("\nTotal postive numbers=%d\n", posCount);
+    for (int i = 0; i < posCount; i++)
+        printf(" %d,", positive[i]);
 
-    printf("\nTotal negative numbers=%d\n",negCount);
-    for(int i=0;i<negCount;i++) printf(" %d,",negative[i]);
+    printf("\nTotal negative numbers=%d\n", negCount);
+    for (int i = 0; i < negCount; i++)
+        printf(" %d,", negative[i]);
 
-    printf("\nTotal zeros=%d\n",zeroCount);
-    for(int i=0;i<zeroCount;i++) printf(" %d,",zeros[i]);
+    printf("\nTotal zeros=%d\n", zeroCount);
+    for (int i = 0; i < zeroCount; i++)
+        printf(" %d,", zeros[i]);
 }
