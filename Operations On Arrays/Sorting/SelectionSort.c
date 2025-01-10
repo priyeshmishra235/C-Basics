62c6cb2520d8a12f5a78c024c29135313a211c4e
Priyesh Mishra <mpriyesh235@gmail.com>
2025-01-11 00:20:03 +0530

    learning vim in vscode

--- Operations On Arrays/Sorting/SelectionSort.c
+++ Operations On Arrays/Sorting/SelectionSort.c
@@ -1,33 +1,27 @@
 #include <stdio.h>
 
-void selectionsort(int arr[], int n)
-{
-    for (int i = 0; i <= n - 2; i++)
-    {
-        int currSmallest = i;
-        for (int j = i + 1; j <= n - 1; j++)
-        {
-            if (arr[currSmallest] > arr[j])
-                currSmallest = j;
-        }
-        if (currSmallest != i)
-        {
-            int temp = arr[i];
-            arr[i] = arr[currSmallest];
-            arr[currSmallest] = temp;
-        }
+void selectionsort(int arr[], int n) {
+  for (int i = 0; i <= n - 2; i++) {
+    int currSmallest = i;
+    for (int j = i + 1; j <= n - 1; j++) {
+      if (arr[currSmallest] > arr[j]) currSmallest = j;
     }
+    if (currSmallest != i) {
+      int temp = arr[i];
+      arr[i] = arr[currSmallest];
+      arr[currSmallest] = temp;
+    }
+  }
 
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
+  for (int i = 0; i < n; i++) printf("%d ", arr[i]);
 }
 
-int main()
-{
-    int arr[] = {755288, 493124, 786116, 738923, 768427, 856419, 62969, 75752, 917346, 720866};
+int main() {
+  int arr[] = {755288, 493124, 786116, 738923, 768427,
+               856419, 62969,  75752,  917346, 720866};
 
-    int n = sizeof(arr) / sizeof(arr[0]);
-    selectionsort(arr, n);
+  int n = sizeof(arr) / sizeof(arr[0]);
+  selectionsort(arr, n);
 
-    return 0;
+  return 0;
 }
--- Without Pointers/LibraryManagement.c
+++ Without Pointers/LibraryManagement.c
@@ -1,35 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-/*Add a Book: Prompt the user to enter details of a book and store it in the array.
-Display All Books: Display the details of all books currently in the library.
-Search for a Book: Allow the user to search for a book by its id or title.
-Delete a Book: Delete a book by its id.
-Update Book Details: Update the details of a book by its id.*/
-struct book
-{
-    int id;
-    char title[30], author[20];
-    float price;
+/*Key Features:
+Add a New Book
+Remove a Book
+Search for a Book
+Borrow a Book
+Return a Book
+Display Available Books
+Display Borrowed Books
+File Handling (Optional for data persistence)*/
+struct book {
+  int id;
+  char title[30], author[20];
+  float price;
 };
-void addBooks(int currentBooks,struct book *book)
-{
-    printf("Enter the id of book: ");
-    scanf("%d", book[currentBooks].id);
-    printf("Enter the title of the book: ");
-    scanf("%s", book[currentBooks].title[30]);
-    printf("Enter the Name of book author: ");
-    scanf("%s", book[currentBooks].author[20]);
-    printf("Enter the price of the book: ");
-    scanf("%f", book[currentBooks].price);
-    --currentBooks;
-}
-int main()
-{
-    int currentBooks = 100;
-    struct book book[currentBooks];
-    addBooks(currentBooks,&book[currentBooks]);
-    printf("%d",book->id);
+struct borrowedBook {
+  int id;
+  char title[30], author[20];
+  float price;
+};
 
-    return 0;
+void addBooksInfo(struct book *book) {
+  printf("Enter the id of book: ");
+  scanf("%d", &book->id);
+  printf("Enter the title of the book: ");
+  scanf("%29s", &book->title);
+  printf("Enter the Name of book author: ");
+  scanf("%19s", &book->author);
+  printf("Enter the price of the book: ");
+  scanf("%f", &book->price);
+}
+int main() {
+  int currentNumberOfBooks = 0;
+  while (1) {
+    int option;
+    printf("Library management system");
+    printf("Select a option from 1 to 10");
+    printf(
+        "1.Add Book details"
+        "\n2.Update book details"
+        "\n3.Show all books");
+    scanf("%d", &option);
+
+    switch (option) {
+      case 1: {
+        int num;
+        printf("How many books you want to add?");
+        scanf("%d", &num);
+        struct book books[currentNumberOfBooks + num];
+        for (int i = currentNumberOfBooks + 1; i <= currentNumberOfBooks + num;
+             i++) {
+          addBooksInfo(&books[i]);
+        }
+        currentNumberOfBooks = currentNumberOfBooks + num;
+        break;
+      }
+      case 3: {
+        struct book books[currentNumberOfBooks];
+        for (int i = 0; i < currentNumberOfBooks; i++) {
+          printf(
+              "Book number %d-->\nTitle: %s\n Author: %s\n Id: %d\n Price: "
+              "%f\n",
+              i, books[i].title, books[i].author, books[i].id, books[i].price);
+        }
+        break;
+      }
+    }
+  }
+
+  return 0;
 }
